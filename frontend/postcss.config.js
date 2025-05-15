d6a9b1a66028e8f9a1798271f547fda5877b7f28
pivnati <natanmuleta911@gmail.com>
2025-05-15 11:28:18 -0700

    create the blogroute page

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,6 @@
+/* eslint-disable no-undef */
+import tailwindcss from '@tailwindcss/postcss';
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [tailwindcss(), require('autoprefixer')],
+};
